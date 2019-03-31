1f85c9a0c040865679646b2688b023ff7dfde268
Russell Blickhan <rwblickhan@gmail.com>
2019-03-31 16:12:40 -0700

    Return submission successful code

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 """Main Lambda handler."""
 
+import json
 import logging
 
 
-def handler(event, context):
+def handler(event, _context):
     """Handle Lambda events."""
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    logger.info("Started CS Coffee Chat!")
+    logger.info("Serving CS Coffee Chat request!")
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Submission successful!")
+    }
