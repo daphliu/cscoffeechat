2908323bfd830732b3f9621205801e74c679c55f
Daphne Liu <daphnel15113455@gmail.com>
2019-03-31 17:01:05 -0700

    WIP setting up service in google sheet

--- google_sheet.py
+++ google_sheet.py
@@ -1,9 +1,22 @@
 """Interface to Google Sheets."""
+from oauth2client.client import GoogleCredentials
+from googleapiclient import errors
+from googleapiclient.discovery import build
 from typing import List, NamedTuple
+import httplib2
 
-SPREADSHEET_ID = '...'
-RANGE_NAME = '...'
+SPREADSHEET_ID = '1ejbnTskarzX0VgJonjn2wGrdOmKV4k-75itr4mPLcQU'
+RANGE_NAME = ''
 
+credentials = create_credentials() 
+http = credentials.authorize(httplib2.Http())
+credentials.refresh(http)
+service = build('sheets', 'v4', http=http)
+
+def create_credentials():
+    credentials = GoogleCredentials.get_application_default()
+    scope = ['https://www.googleapis.com/auth/drive']
+    return credentials.create_scoped(scope)
 
 class FormResult(NamedTuple):
     """Class representing data users enter into coffee chat form."""
@@ -21,15 +34,17 @@ class FormResult(NamedTuple):
 
 def save_form_result(form_result):
     """Save form result to Google Sheets."""
-    values = [list(form_result)]
+    try: 
+        values = [list(form_result)]
+        body = {
+            'values': values
+        }
+        result = service.spreadsheets().values.append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=RANGE_NAME,
+            valueInputOption="RAW",
+            body=body_
+        ).execute()
 
-    body = {
-        'values': values
-    }
-
-    result = service.spreadsheets().values.append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=RANGE_NAME,
-        valueInputOption="RAW",
-        body=body
-    ).execute()
+    except: 
+        raise Exception('Oh no, something terrible happened.')
